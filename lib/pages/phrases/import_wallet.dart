25cde72e181bc3dbd55e750d8729fa8014102bd3
HosseinAsadi <HosseinA.9876@gmail.com>
2021-08-21 13:40:56 +0430

    modifying import_wallet ui

--- lib/pages/phrases/import_wallet.dart
+++ lib/pages/phrases/import_wallet.dart
@@ -4,6 +4,7 @@ import 'package:digit41/hive/app_hive.dart';
 import 'package:digit41/hive/wallet_model.dart';
 import 'package:digit41/utils/app_permission.dart';
 import 'package:digit41/utils/app_state_management.dart';
+import 'package:digit41/utils/images_path.dart';
 import 'package:digit41/utils/strings.dart';
 import 'package:digit41/utils/utils.dart';
 import 'package:digit41/widgets/app_bottom_navigation.dart';
@@ -110,57 +111,87 @@ void importWallet() async {
         child: child,
       );
 
-  bottomSheet(
-    Strings.IMPORT.tr + ' ' + Strings.WALLET.tr,
-    frontOfTitle: GetPlatform.isWeb
-        ? null
-        : Row(
+  Get.bottomSheet(
+    SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 32.0),
+        child: Form(
+          key: formKey,
+          child: Column(
             mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconButton(
-                onPressed: () async {
-                  String re = await barcodeScan();
-                  phrases.controller.text = re;
-                },
-                icon: Icon(Icons.qr_code_scanner, color: Colors.blue),
+              const SizedBox(height: 8.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      Strings.IMPORT.tr + ' ' + Strings.WALLET.tr,
+                      style: TextStyle(
+                        color: Get.theme.primaryColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      String re = await barcodeScan();
+                      phrases.controller.text = re;
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          Images.SCAN,
+                          width: 20.0,
+                          height: 20.0,
+                          color: Get.theme.primaryColor,
+                        ),
+                        const SizedBox(width: 4.0),
+                        Text(
+                          Strings.SCAN_QR.tr,
+                          style: TextStyle(color: Get.theme.primaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(Strings.SCAN_QR.tr),
-            ],
-          ),
-    child: Padding(
-      padding: const EdgeInsets.only(top: 24.0, bottom: 16.0),
-      child: Form(
-        key: formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            phrases,
-            const SizedBox(height: 8.0),
-            txt(
-              Text(
-                Strings.DESC_IMPORT_WALL.tr,
-                style: TextStyle(fontSize: 12.0),
+              const SizedBox(height: 16.0),
+              phrases,
+              const SizedBox(height: 8.0),
+              txt(
+                Text(
+                  Strings.DESC_IMPORT_WALL.tr,
+                  style: TextStyle(fontSize: 12.0),
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            txt(Text(Strings.WALLET_NAME.tr)),
-            const SizedBox(height: 8.0),
-            name,
-            const SizedBox(height: 32.0),
-            txt(
+              const SizedBox(height: 16.0),
+              txt(Text(Strings.WALLET_NAME.tr)),
+              const SizedBox(height: 8.0),
+              name,
+              const SizedBox(height: 32.0),
               TextButton(
                 onPressed: () {},
                 child: Text(Strings.Q_IMPORT_WALL.tr),
               ),
-            ),
-            const SizedBox(height: 12.0),
-            AppButton(
-              title: Strings.IMPORT.tr,
-              onTap: import,
-              btnColor: Get.theme.primaryColor,
-            ),
-          ],
+              AppButton(
+                title: Strings.IMPORT.tr,
+                onTap: import,
+                btnColor: Get.theme.primaryColor,
+              ),
+            ],
+          ),
         ),
+      ),
+    ),
+    isScrollControlled: true,
+    backgroundColor: Get.theme.bottomSheetTheme.backgroundColor,
+    shape: const RoundedRectangleBorder(
+      borderRadius: const BorderRadius.only(
+        topLeft: const Radius.circular(24.0),
+        topRight: const Radius.circular(24.0),
       ),
     ),
   );
