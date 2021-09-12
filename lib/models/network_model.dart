82fe28ecec30e4dd54efebca2783c10b11cc26f6
HosseinAsadi <haviking76@gmail.com>
2021-09-12 15:47:59 +0430

    network model was completed

--- lib/models/network_model.dart
+++ lib/models/network_model.dart
@@ -9,13 +9,29 @@ class NetworkModel extends HiveObject {
   String? url;
 
   @HiveField(2)
+  int? chainId;
+
+  @HiveField(3)
+  String? currencySymbol;
+
+  @HiveField(4)
+  String? blockExplorerURL;
+
+  @HiveField(5)
   bool selected;
 
+  /// this field is for detect user networks from default networks
+  @HiveField(6)
+  bool byUser;
 
   NetworkModel({
     this.name,
     this.url,
+    this.chainId,
+    this.currencySymbol,
+    this.blockExplorerURL,
     this.selected = false,
+    this.byUser = false,
   });
 }
 
@@ -33,19 +49,31 @@ class NetworkAdapter extends TypeAdapter<NetworkModel> {
     return NetworkModel(
       name: fields[0],
       url: fields[1],
-      selected: fields[2],
+      chainId: fields[2],
+      currencySymbol: fields[3],
+      blockExplorerURL: fields[4],
+      selected: fields[5],
+      byUser: fields[6],
     );
   }
 
   @override
   void write(BinaryWriter writer, NetworkModel obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(7)
       ..writeByte(0)
       ..write(obj.name)
       ..writeByte(1)
       ..write(obj.url)
       ..writeByte(2)
-      ..write(obj.selected);
+      ..write(obj.chainId)
+      ..writeByte(3)
+      ..write(obj.currencySymbol)
+      ..writeByte(4)
+      ..write(obj.blockExplorerURL)
+      ..writeByte(5)
+      ..write(obj.selected)
+      ..writeByte(6)
+      ..write(obj.byUser);
   }
 }
